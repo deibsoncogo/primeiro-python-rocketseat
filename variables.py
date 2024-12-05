0ecec37643a640be72c2906051490834ce02b584
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-05 20:01:00 -0300

    feat: criando variáveis do tipo int e float

---
+++ variables.py
@@ -0,0 +1,13 @@
+# variável utilizando o padrão camelCase
+fullName = "Gabriel"
+
+# variável utilizando o padrão snake_case
+full_name = "Gabriel"
+
+# número inteiro
+numberInt = 25
+print("Número do tipo", type(numberInt), "e seu valor é", numberInt)
+
+# número flutuante
+numberFloat = 3.14
+print("Número do tipo", type(numberFloat), "e seu valor é", numberFloat)
