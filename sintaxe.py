03f357f76e3b02f54515960a00f8dadefbddd18e
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-05 20:00:00 -0300

    feat: criando as primeiras variáveis

---
+++ sintaxe.py
@@ -0,0 +1,11 @@
+# criando um comentário de uma linha
+
+""" comentário de múltiplas linhas
+uma nova linha
+"""
+
+# permite unificar com a linha de baixo
+name = "Deibson" + \
+" Cogo"
+
+print(name)
---
+++ variable.py
@@ -0,0 +1,7 @@
+# variável utilizando o padrão camelCase
+fullName = "Gabriel"
+
+# variável utilizando o padrão snake_case
+full_name = "Gabriel"
+
+age = 25
