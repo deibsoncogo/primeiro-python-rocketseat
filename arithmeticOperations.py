e6dbdf23e645b548714a38bc8a569482440f4487
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-05 20:02:00 -0300

    feat: realizando operações aritméticas

---
+++ arithmeticOperations.py
@@ -0,0 +1,18 @@
+# realizando operações aritméticas
+arithmeticOperations = 5 + 2 # soma
+print("5 + 2 =", arithmeticOperations)
+
+arithmeticOperations = 5 - 2 # subtração
+print("5 - 2 =", arithmeticOperations)
+
+arithmeticOperations = 5 * 2 # multiplicação
+print("5 * 2 =", arithmeticOperations)
+
+arithmeticOperations = 5 / 2 # divisão
+print("5 / 2 =", arithmeticOperations)
+
+arithmeticOperations = 5 // 2 # divisão com resultado inteiro
+print("5 // 2 =", arithmeticOperations)
+
+arithmeticOperations = 5 % 2 # resto da divisão
+print("5 % 2 =", arithmeticOperations)
