85cea40313b6082529e3f2f2028c1395e415a7aa
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-06 20:00:00 -0300

    feat: aprendendo as principais função de texto

---
+++ functionsText.py
@@ -0,0 +1,22 @@
+name = "Gabriel Grande"
+
+print("Codifica para bit:", name.encode())
+print("Decodifica de bit:", name.encode().decode())
+
+print("Texto em maiúsculo:", name.upper())
+print("Texto em minúsculo:", name.lower())
+
+print("Conta quantos a existem:", name.count("a"))
+print("Retornar a posição do primeiro a:", name.find("a"))
+
+print("Substitui o texto:", name.replace("Grande", "G."))
+print("Cria um array ao encontra o elemento:", name.split(" "))
+print("Insere o elemento dentro do texto:", "-".join(name))
+
+print("Se o texto tem x no começo e fim:", name.strip("x"))
+print("Se o texto tem x no fim:", name.rstrip("x"))
+
+print("Se o texto começa com Ga:", name.startswith("Ga"))
+
+print("Se o texto tem el:", "el" in name)
+print("Se o texto não tem el:", "el" not in name)
