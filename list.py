5ff20c330920907891fdc9f2b04d22a63325d1e2
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-07 20:00:00 -0300

    feat: aprendendo a criar e utilizar listas

---
+++ list.py
@@ -0,0 +1,8 @@
+# criando uma lista (array)
+myList = [1, 2, 3, 4, 5, "rocketseat", True, False]
+
+# formas de exibir a lista
+print("Minha lista, completa:", myList)
+print("Minha lista, 6º elemento:", myList[5])
+print("Minha lista, 2º ao 5º elemento:", myList[1:5])
+print("Minha lista, a partir do 2º elemento:", myList[1:])
