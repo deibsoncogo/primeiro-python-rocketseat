4833ffe60e55b8f8545e04fc68b953c8f52d185c
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-09 20:00:00 -0300

    feat: criando uma tupla

---
+++ tupla.py
@@ -0,0 +1,7 @@
+# um tupla é ordenada e imutável
+myTupla = (1, 2, 2, 3, 4)
+
+print("Terceiro elemento:", myTupla[2])
+print("Último elemento:", myTupla[-1])
+
+print("Conta quantos números 2 existem:", myTupla.count(2))
