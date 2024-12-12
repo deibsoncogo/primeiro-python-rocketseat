54db6a2ffef63a6f24627e71d42ad04da61c9aa3
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-12 20:00:00 -0300

    feat: criação do menu do programa gerenciador de tarefas

---
+++ manager.py
@@ -0,0 +1,15 @@
+while True:
+  print("\nMenu do Gerenciador de Lista de Tarefas:")
+  print("1. Adicionar tarefas")
+  print("2. Ver tarefas")
+  print("3. Atualizar tarefa")
+  print("4. Completar tarefa")
+  print("5. Deletar tarefas concluídas")
+  print("6. Sair")
+
+  select = input("\nDigite a sua escolha: ")
+
+  if select == "6":
+    break
+
+print("\nPrograma finalizado")
