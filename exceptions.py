9bd8620d103bd33cf53e241924b541dff802c2e6
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-16 20:00:00 -0300

    feat: aprendendo a lidar com exceções

---
+++ exceptions.py
@@ -0,0 +1,19 @@
+print("Exemplo de captura de exceções")
+
+try:
+  number = int(input("Digite um número inteiro: "))
+
+  result = 10 / number
+
+except ValueError as error:
+  print(f"Error value: {error}")
+  raise ValueError("Tipo de valor deve ser um número inteiro")
+
+except Exception as error:
+  print(f"Error: {error}")
+
+else:
+  print(f"O resultado foi de {result}")
+
+finally:
+  print("Operação finalizada")
