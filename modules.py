b03fc255591bc698333e2bb6a373651d11b09309
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-16 20:01:00 -0300

    feat: aprendendo a utilizar módulos

---
+++ modules.py
@@ -0,0 +1,9 @@
+# exemplo de importação de um módulo padrão
+import math
+raiz = math.sqrt(25)
+print(f"A raiz quadrada de 25 é {raiz}")
+
+# exemplo de importação de um módulo especifico
+from math import sqrt
+raiz = sqrt(196)
+print(f"A raiz quadrada de 196 é {raiz}")
